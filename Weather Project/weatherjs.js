55156e28541992a9fe2655fda8f44dc6b2b737e8
hoppenic <hoppenic@gmail.com>
2018-04-21 18:37:23 -0500

    update

--- Weather Project/weatherjs.js
+++ Weather Project/weatherjs.js
@@ -4,7 +4,9 @@ $(document).ready(function(){
 $("#mybutton").click(function(){
     $.getJSON('https://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="chicago, il")&format=json',function(jd){
         console.log(jd);
+    
         
+   
     var myCity=jd.query.results.channel.location.city;
             document.write("The ten day forecast for "+myCity + ",");
         
@@ -15,19 +17,10 @@ $("#mybutton").click(function(){
         var today = jd.query.results.channel.item.forecast[i];        document.write("Day: " + today.date+ " ");
          document.write("high of "+today.high + " ");
          document.write("low of "+today.low + " <br>");
+    }   
+                                                                                   
     }
-        
-        
-       
-      
-                                                                              
-            
-        
-    }
-        
               
-       
-       
         
         
         //$('#result').html(jd);});
@@ -35,3 +28,34 @@ $("#mybutton").click(function(){
     
     )})
 });
+
+
+
+$("#myTextBox").click(function(){
+    $.getJSON('https://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="san diego, ca")&format=json',function(jd){
+        console.log(jd);
+    
+        
+   
+    var myCity=jd.query.results.channel.location.city;
+            document.write("The ten day forecast for "+myCity + ",");
+        
+    var myRegion=jd.query.results.channel.location.region;
+            document.write(myRegion + ": "+"<br>");
+        
+    for(var i=0;i<jd.query.results.channel.item.forecast.length;i++){
+        var today = jd.query.results.channel.item.forecast[i];        document.write("Day: " + today.date+ " ");
+         document.write("high of "+today.high + " ");
+         document.write("low of "+today.low + " <br>");
+    }   
+                                                                                   
+    }
+              
+              
+  
+        
+        
+        //$('#result').html(jd);});
+    
+    
+    )})
