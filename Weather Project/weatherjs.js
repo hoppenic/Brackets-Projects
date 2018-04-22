c9a622829a0d2237cac672ea61348d1cf833c257
hoppenic <hoppenic@gmail.com>
2018-04-22 09:30:29 -0500

    update

--- Weather Project/weatherjs.js
+++ Weather Project/weatherjs.js
@@ -32,30 +32,21 @@ $("#mybutton").click(function(){
 
 
 $("#myTextBox").click(function(){
-    $.getJSON('https://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="san diego, ca")&format=json',function(jd){
-        console.log(jd);
+    $.getJSON('https://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="san diego, ca")&format=json',function(xy){ $('#result').html(xy);});
+      
     
         
    
-    var myCity=jd.query.results.channel.location.city;
-            document.write("The ten day forecast for "+myCity + ",");
-        
-    var myRegion=jd.query.results.channel.location.region;
-            document.write(myRegion + ": "+"<br>");
-        
-    for(var i=0;i<jd.query.results.channel.item.forecast.length;i++){
-        var today = jd.query.results.channel.item.forecast[i];        document.write("Day: " + today.date+ " ");
-         document.write("high of "+today.high + " ");
-         document.write("low of "+today.low + " <br>");
-    }   
+   
+    }   )
                                                                                    
-    }
+    
               
               
   
         
         
-        //$('#result').html(jd);});
+      
     
     
-    )})
+
