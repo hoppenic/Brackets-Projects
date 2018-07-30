78cb2d9578249b7c6231e4c2b454304d3b81fb51
hoppenic <hoppenic@gmail.com>
2018-07-30 14:55:28 -0500

    updates

---
+++ test website/Script.js
@@ -0,0 +1,17 @@
+function Course(title,instructor,level,published,views){
+    this.title=title;
+    this.instructor=instructor;
+    this.published=published;
+    this.view=views;
+    
+    
+    
+    
+}
+
+
+
+var course01=new Course("js","me",2019,50);
+
+con
+
